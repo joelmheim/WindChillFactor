fa09dfae27df23b7eb8d9df06a9f3d78e6985acf
Jørn Ølmheim <jorn@olmheim.com>
2012-05-12 15:16:16 +0200

    Added redirect

--- web.js
+++ web.js
@@ -4,26 +4,26 @@ var express = require('express')
 
 var app = express.createServer(express.logger());
 
-//app.configure(function(){
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'jade');
-//  app.use(express.bodyParser());
-//  app.use(express.methodOverride());
-//  app.use(app.router);
-//  app.use(express.static(__dirname + '/public'));
-//});
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
 
-//app.configure('development', function(){
-//  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-//});
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-//app.configure('production', function(){
-//  app.use(express.errorHandler());
-//});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 app.get('/', function(req, resp) {
-//    resp.redirect('/index.html');
-  resp.send('Hello World!');
+  resp.redirect('/index.html');
+//  resp.send('Hello World!');
 });
 
 var port = process.env.PORT || 3001;
