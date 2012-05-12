8d51208d0aa3a6bc698e8bd869874247e7e3bb1e
Jørn Ølmheim <jorn@olmheim.com>
2012-05-12 15:03:00 +0200

    Updated config

--- web.js
+++ web.js
@@ -2,34 +2,31 @@ var express = require('express')
   , routes = require('./routes');
 
 
-var app = module.exports = express.createServer(express.logger());
+var app = express.createServer(express.logger());
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+//app.configure(function(){
+//  app.set('views', __dirname + '/views');
+//  app.set('view engine', 'jade');
+//  app.use(express.bodyParser());
+//  app.use(express.methodOverride());
+//  app.use(app.router);
+//  app.use(express.static(__dirname + '/public'));
+//});
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+//app.configure('development', function(){
+//  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+//});
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+//app.configure('production', function(){
+//  app.use(express.errorHandler());
+//});
 
 app.get('/', function(req, resp) {
-    resp.redirect('/index.html');
+//    resp.redirect('/index.html');
+  resp.send('Hello World!');
 });
 
 var port = process.env.PORT || 3001;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
