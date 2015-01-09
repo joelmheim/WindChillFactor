4637af9a88f2bc5ddbbfdf416ecac544a42278a6
Jørn Ølmheim <joe@statoil.com>
2015-01-09 13:49:33 +0100

    added angular scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name windChillAngularApp
+ * @description
+ * # windChillAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('windChillAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name windChillAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the windChillAngularApp
+ */
+angular.module('windChillAngularApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
