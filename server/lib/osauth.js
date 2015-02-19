fb73eb4326f8339889bc4596a3c560904487ad7d
Jørn Ølmheim <joe@statoil.com>
2015-02-20 00:15:48 +0100

    Fixed jshint suggestions.

--- server/lib/osauth.js
+++ server/lib/osauth.js
@@ -16,12 +16,12 @@ var OSAuth = OSAuth || (function() {
         },
         OSATimedHash: function(n, a) {
             var e = OSAuth.OSAHash(n, a),
-                r = parseInt(Math.floor((new Date).getTime() / 6e4)).toString();
-            return HmacSHA1(r, e).toString()
+                r = parseInt(Math.floor((new Date()).getTime() / 6e4)).toString();
+            return HmacSHA1(r, e).toString();
         },
         OSASaltedHash: function(n, a, e) {
             var r = OSAuth.OSAHash(n, a);
-            return HmacSHA1(e, r).toString()
+            return HmacSHA1(e, r).toString();
         },
         OSAGetChallenge: function(t) {
             return $.ajax({
@@ -31,12 +31,12 @@ var OSAuth = OSAuth || (function() {
                 dataType: "json",
                 aysnc: !0,
                 cache: !1
-            })
+            });
         },
         OSASalt: function(t) {
             void 0 == t && (t = 64);
             for (var n = "", a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", e = 0; t > e; e++) n += a.charAt(Math.floor(Math.random() * a.length));
-            return n
+            return n;
         },
         doLogin: function(email, password) {
             return OSAuth.OSAGetChallenge(email).then(function (resp) {
@@ -52,16 +52,15 @@ var OSAuth = OSAuth || (function() {
                 });
             }).then(function (resp) {
                 var deferred = $.Deferred();
-                sessionStorage['skey'] = resp.skey;
-                sessionStorage['user_id'] = resp.user_id;
-                sessionStorage['user'] = JSON.stringify(resp.user);
-                sessionStorage['name'] = resp.user.first_name + " " + resp.user.last_name;
+                sessionStorage.setItem('skey', resp.skey);
+                sessionStorage.setItem('user_id', resp.user_id);
+                sessionStorage.setItem('user', JSON.stringify(resp.user));
+                sessionStorage.setItem('name', resp.user.first_name + " " + resp.user.last_name);
                 deferred.resolve(resp);
                 return deferred;
             }).fail(function (resp) {
                 console.log(resp);
             });
         }
-
-    }
+    };
 })();
