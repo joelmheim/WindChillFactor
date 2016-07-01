f1c9987172894f6835c3ec14846f44b47e2ed75f
Jørn Ølmheim <joe@statoil.com>
2016-07-01 14:45:22 +0200

    Clean up some log messages and adjusted zoom levels.

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,8 +9,6 @@
  */
 angular.module('windchillApp')
   .controller('MainCtrl', ['$scope', '$http', 'leafletData', 'WeatherService', function ($scope, $http, leafletData, WeatherService) {
-    console.log("DEBUG: MainCtrl init");
-
     function centerZoomAndAddMarker(position, zoom) {
       angular.extend($scope, {
         position: position,
@@ -32,6 +30,7 @@ angular.module('windchillApp')
     }
 
     leafletData.getMap().then(function(map) {
+      // Statens Kartverk is not supported by angular leaflet plugin (Possible contribution?) so I have to do it like this
       var statkart_cache = new L.TileLayer.WMS("https://opencache.statkart.no/gatekeeper/gk/gk.open", {
           layers: 'topo2',
           format: 'image/png',
@@ -40,36 +39,15 @@ angular.module('windchillApp')
           attribution: "Kartverket"
       });
       statkart_cache.addTo(map);
-      map.locate({ setView: true, maxZoom: 12 });
+      map.locate({ setView: true, maxZoom: 16 });
     });
+
     var position = {
           lat: 63.43048590647616,
           lng: 10.395147800445558
     };
 
     angular.extend($scope, {
-      // layers: {
-      //   baselayers: {
-      //     osm: {
-      //       name: 'OpenStreetMap',
-      //       url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      //       type: 'xyz'
-      //     }
-      //   },
-      //   overlays: {
-      //     wms: {
-      //       name: 'OpenCycleMap',
-      //       type: 'xyz',
-      //       visible: true,
-      //       url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
-      //       layerParams: {
-      //         format: 'image/png',
-      //         transparent: true,
-      //         opacity: 0.5
-      //       }
-      //     }
-      //   }
-      // },
       text: 'Initial position ',
       events: { }
     });
@@ -79,14 +57,12 @@ angular.module('windchillApp')
     centerZoomAndAddMarker(position, 12);
 
     $scope.$on('leafletDirectiveMap.locationfound', function (event, args) {
-      console.log("DEBUG: Location found: ", event, args);
       var e = args.leafletEvent;
       WeatherService.position = e.latlng;
-      centerZoomAndAddMarker(e.latlng, 15);
+      centerZoomAndAddMarker(e.latlng, 16);
     });
 
     $scope.$on('leafletDirectiveMap.locationerror', function (event, args) {
-      console.log("DEBUG: Location error", event, args);
       var e = args.leafletEvent;
       leafletData.getMap().then(function (map) {
         L.marker(e.latlng).addTo(map)
@@ -95,12 +71,10 @@ angular.module('windchillApp')
     });
 
     $scope.$on('leafletDirectiveMap.click', function (event, args) {
-      console.log("DEBUG: Location selected: ", event, args);
       var e = args.leafletEvent;
       WeatherService.position = e.latlng;
-      centerZoomAndAddMarker(e.latlng, 15);
+      centerZoomAndAddMarker(e.latlng, 16);
       leafletData.getMarkers().then(function(markers) {
-        console.log('DEBUG: Marker: ', markers);
         markers.m1.openPopup();
       });
     });
--- client/app/scripts/controllers/weather.js
+++ client/app/scripts/controllers/weather.js
@@ -10,7 +10,6 @@
 angular.module('windchillApp')
   .controller('WeatherCtrl', ['$scope', '$http', 'leafletData', 'WeatherService', function ($scope, $http, leafletData, WeatherService) {
 
-    console.log("DEBUG: WeatherCtrl init");
     WeatherService.currentPositionWeather()
       .success(function(data) {
         angular.extend( $scope, {
